180ec53b052bf9e599c97a59d2f6beb1575dfabb
herodote2242 <matthias.gil@hotmail.fr>
2018-11-08 13:50:09 +0100

    parser is ready

--- sentence_manipulator.py
+++ sentence_manipulator.py
@@ -3,26 +3,32 @@
 
 import fr_stop_words
 
+
 class SentenceManipulator:
     """
     This class modifies the written message by removing punctuation,
     accents, capitals and chitchat words in order to keep only
     the key words.
     """
+
     def __init__(self, question=None):
         self.question = question
-    
-    def lower_case(self):
+
+    def lower_case(self, question=None):
         """
-        Convert the message in lower case.
+        Converts the message in lower case.
         """
-        self.question = self.question.lower()
+        if question:
+            self.question = question
+        self.question = (str(self.question)).lower()
         return self.question
-        
+
     def suppress_accents(self, question=None):
         """
         This functions removes the french accents.
         """
+        if question:
+            self.question = question
         for accent in "àâä":
             self.question = self.question.replace(accent, "a")
         for accent in "éèêë":
@@ -41,25 +47,56 @@ class SentenceManipulator:
         """
         This function removes the punctuation marks.
         """
+        if question:
+            self.question = question
         for punctuation in ",?!\\;\"./:'&~{[(-|`_^@)]=}+*<>²%$£€µ§°¤":
-            self.question = self.question.replace(punctuation, "")
+            self.question = self.question.replace(punctuation, " ")
             self.question = self.question.strip()
         return self.question
 
     def cut_question(self, question=None):
         """
-        Separate each word of the message and return them into a list.
+        Separates each word of the message and return them into a list.
         """
+        if question:
+            self.question = question
         self.question = self.question.split()
         return self.question
 
-    def no_stop_words(self, question=None):
+    def suppress_stop_words(self, question=None):
         """
-        remove the words if they are stopwords
+        Removes the words if they are part of the stopwords'list.
         """
-        for stopword in fr_stop_words.list_of_stop_words:
-            try:
-                self.question = self.question.remove(stopword)
-            except:
-                pass
-        return self.question
+        if question:
+            self.question = question
+        words = []
+        for word in self.question:
+            if word not in set(fr_stop_words.list_of_stop_words):
+                words.append(word)
+        self.question = words
+        return words
+
+    def clean_sentence(self, question=None):
+        """
+        Launches the different manipulations of a sentence.
+        """
+        self.question = question
+        self.lower_case()
+        self.suppress_accents()
+        self.suppress_punctuation()
+        self.cut_question()
+        self.suppress_stop_words()
+        return " ".join(self.question)
+
+
+def main():
+    """
+    Launches the module.
+    """
+    main = SentenceManipulator()
+    question = main.clean_sentence()
+    return question
+
+
+if __name__ == "__main__":
+    main()
--- test_sentence_manipulator.py
+++ test_sentence_manipulator.py
@@ -2,8 +2,8 @@
 # -*- coding: Utf-8 -*
 
 import pytest
-import fr_stop_words
 import sentence_manipulator as manipulator
+
 
 class TestSentenceManipulator:
 
@@ -11,22 +11,15 @@ class TestSentenceManipulator:
     def setup_class(cls):
         cls.manip = manipulator.SentenceManipulator()
 
-    #@pytest.mark.parametrize("chaine, result", [
-    #    ("Chaïne chaine !", ["chaine", "chaine"]),
-    #    ("!Hello Papy, où se trouve la basilique de Fourvière ?",
-    #        ["trouve", "basilique", "fourviere"]),
-    #    ("stp Grandpy, tu connais ça toi, l'adresse de la tour Crayon à Lyon?",
-    #        ["adresse", "tour", "crayon", "lyon"])
-    #    ])
-
     def test_lower_case(self):
         self.manip.question = "Chaïne chaine !"
         assert self.manip.lower_case() == "chaïne chaine !"
 
     def test_suppress_accents(self):
         self.manip.question = "chaïne chaine !"
-        assert self.manip.suppress_accents(self.manip.question) == "chaine chaine !"
-        
+        assert self.manip.suppress_accents(
+            self.manip.question) == "chaine chaine !"
+
     def test_suppress_punctuation(self):
         self.manip.question = "chaine chaine !"
         assert self.manip.suppress_punctuation() == "chaine chaine"
@@ -35,6 +28,16 @@ class TestSentenceManipulator:
         self.manip.question = "chaine chaine !"
         assert self.manip.cut_question() == ['chaine', 'chaine', '!']
 
-    def test_no_stop_words(self):
+    def test_suppress_stop_words(self):
         self.manip.question = ['chaine', 'chaine']
-        assert self.manip.no_stop_words() == ['chaine', 'chaine']
+        assert self.manip.suppress_stop_words() == ['chaine', 'chaine']
+
+    @pytest.mark.parametrize("chaine, result", [
+        ("Chaïne chaine !", "chaine chaine"),
+        ("!Hello Papy, où se trouve la basilique de Fourvière ?",
+            "trouve basilique fourviere"),
+        ("stp Grandpy, tu connais ça toi, l'adresse de la tour Crayon à\
+            Lyon?", "adresse tour crayon lyon")
+    ])
+    def test_clean_sentence(self, chaine, result):
+        assert self.manip.clean_sentence(chaine) == result
