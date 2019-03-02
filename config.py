41174d10d238ac82226a946a1ce8d053e1ae6a44
herodote2242 <matthias.gil@hotmail.fr>
2019-03-02 11:35:20 +0100

    solving responsive footer, small changes in README/md and config.py

--- config.py
+++ config.py
@@ -5,4 +5,10 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    """
+    To protect web forms of being attacked by cross-site request forgery,
+    this secret key is known only by developpers and maintainers of th app.
+    The first term 'SECRET_KEY' looks for the value in the environment
+    variables, while the second is used if the first is not founded.
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Sf45a21re_f55sfzrTY77-8lzoc'
